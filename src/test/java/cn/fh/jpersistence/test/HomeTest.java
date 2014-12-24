6655ea935390104427f5dfc5c7a19783e9ebc22d
wanghongfei <brucewhf@gmail.com>
2014-12-24 10:18:42 +0800

    test persistence operation of Home

--- src/test/java/cn/fh/jpersistence/test/HomeTest.java
+++ src/test/java/cn/fh/jpersistence/test/HomeTest.java
@@ -7,10 +7,13 @@ import javax.persistence.Persistence;
 import org.junit.Test;
 
 import cn.fh.jpersistence.test.domain.Member;
+import cn.fh.jpersistence.test.domain.MemberHome;
 
 public class HomeTest {
 	@Test
 	public void testJPA() {
+		System.out.println("testing JPA");
+
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("test");
 		EntityManager em = emf.createEntityManager();
 		
@@ -20,5 +23,24 @@ public class HomeTest {
 		em.getTransaction().begin();
 		em.persist(m);
 		em.getTransaction().commit();
+	}
+	
+	@Test
+	public void testHome() {
+		System.out.println("testing Home");
+		MemberHome mHome = new MemberHome();
+		
+		Member m = new Member();
+		m.setName("bruce");
+		mHome.setInstance(m);
+		mHome.setEntityManager(getEntityManager());
+		mHome.persist();
+	}
+	
+	private EntityManager getEntityManager() {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("test");
+		EntityManager em = emf.createEntityManager();
+		
+		return em;
 	}
 }
---
+++ src/test/java/cn/fh/jpersistence/test/domain/MemberHome.java
@@ -0,0 +1,6 @@
+package cn.fh.jpersistence.test.domain;
+
+import cn.fh.jpersistence.Home;
+
+public class MemberHome extends Home<Member> {
+}
