8d8a7f328635223c7eeaa16c56dc06891390f989
wanghongfei <brucewhf@gmail.com>
2014-12-26 13:50:11 +0800

    write Query<T> component. untested

--- src/main/java/cn/fh/jpersistence/Query.java
+++ src/main/java/cn/fh/jpersistence/Query.java
@@ -1,5 +1,64 @@
 package cn.fh.jpersistence;
 
-public abstract class Query<T> {
+import java.util.ArrayList;
+import java.util.List;
 
+public abstract class Query<T> extends AbstractComponent<T> {
+	
+	private List<T> resultList = new ArrayList<T>();
+	
+	private int maxResult = 10;
+	private int curPage = 0;
+	private boolean refresh = false;
+	
+	public List<T> getResultList() {
+		if (null == this.resultList) {
+			determineJpaType();
+			getEntityType();
+
+			fetchResultList();
+		}
+		
+		if (true == refresh) {
+			fetchResultList();
+		}
+
+		return this.resultList;
+	}
+	
+	public void setPage(int page) {
+		this.curPage = page - 1;
+	}
+	
+	public void setMaxResult(int max) {
+		this.maxResult = max;
+	}
+	
+	@Override
+	public void clear() {
+		super.clear();
+		
+		this.maxResult = 10;
+		this.curPage = 0;
+		this.refresh = false;
+		this.resultList = null;
+	}
+	
+	/**
+	 * Set the refresh flag to true. Next invocation of getResultList() will issue 
+	 * a query to database.
+	 */
+	public void refresh() {
+		this.refresh = true;
+	}
+	
+	private void fetchResultList() {
+		this.resultList = this.em.createQuery("SELECT obj FROM " + this.instanceClass.getName() + " obj", this.instanceClass)
+			.setFirstResult(this.maxResult * this.curPage)
+			.setMaxResults(this.maxResult)
+			.getResultList();
+		
+		this.refresh = false;
+	}
+	
 }
